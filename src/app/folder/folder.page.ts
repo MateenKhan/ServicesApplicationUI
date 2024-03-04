3546c0ea556a6570ab56f7cd7f2fcadc6871238f
Mateen <makhan@cendyn.com>
2024-03-05 03:54:38 +0530

    added square feet calculation

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -11,9 +11,18 @@ export class FolderPage implements OnInit {
   private activatedRoute = inject(ActivatedRoute);
   public start!: string;
   public end!: string;
+  public length!: number;
+  public width!: number;
+  public depth!: number;
+  public total!: number;
+
   constructor() { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+  calculate() {
+    this.total = 0;
+    this.total = (2 * (this.length * this.width)) + (2 * (this.width * this.depth)) + (2 * (this.depth * this.length));
   }
 }
